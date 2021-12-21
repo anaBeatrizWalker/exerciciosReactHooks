1f8cdaf0fbe5df1555c4afbe01ff37781b3cddef
Ana Beatriz <anabiawalker@gmail.com>
2021-12-21 16:58:28 -0300

    Adc acesso aos conteúdos do Contexto

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import DataContext, {data} from '../../data/DataContext'
 
 const UseContext = (props) => {
+
+    const context = useContext(DataContext)
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+            <div className="center">
+                {/*Acessando somente o conteúdo de texto do Contexto*/}
+                <span className="text">{context.text}</span>
+                {/*Acessando somente o number do Contexto*/}
+                <span className="text">{context.number}</span>
+            </div>
         </div>
     )
 }
